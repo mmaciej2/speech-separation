02d942c5cade15f7bfab20a474c93618fbbeb48e
Matthew Maciejewski <mmaciej2@jhu.edu>
2019-01-16 17:02:11 -0500

    updated dataset to use datadir and added comments

--- archs/uPIT.py
+++ archs/uPIT.py
@@ -18,14 +18,6 @@ sys.path.append('tools')
 import plot
 
 
-# finds filename for mask corresponding to a mix spec
-def mix_name_to_source_mag_spec_name(filename, index):
-  split = os.path.basename(filename).split('-')
-  if index == 1:
-    return filename.replace("/mix/","/s1/")
-  else:
-    return filename.replace("/mix/","/s2/")
-
 # Define collating function (that constructs packed sequences from a batch)
 class Collator():
 
@@ -57,7 +49,8 @@ class Collator():
 # Define dataset
 class TrainSet(Dataset):
 
-  def __init__(self, filelist, location=""):
+  def __init__(self, datadir, location=""):
+    filelist = datadir+"/feats_train.scp"
     if location:
       with open(filelist) as F:
         indir = os.path.dirname(F.readline().rstrip().split(' ')[1])+'/'
@@ -73,8 +66,8 @@ class TrainSet(Dataset):
 
   def __getitem__(self, idx):
     mix_mag_spec = np.load(self.list[idx])['mix'].transpose()
-    source_mag_spec_1 = np.load(mix_name_to_source_mag_spec_name(self.list[idx], 1))['s1'].transpose()
-    source_mag_spec_2 = np.load(mix_name_to_source_mag_spec_name(self.list[idx], 2))['s2'].transpose()
+    source_mag_spec_1 = np.load(self.list[idx])['s1'].transpose()
+    source_mag_spec_2 = np.load(self.list[idx])['s2'].transpose()
 
     permute_1 = np.concatenate((source_mag_spec_1, source_mag_spec_2), axis=1)
     permute_2 = np.concatenate((source_mag_spec_2, source_mag_spec_1), axis=1)
@@ -84,8 +77,8 @@ class TrainSet(Dataset):
 
 class TestSet(Dataset):
 
-  def __init__(self, filelist):
-    self.list = [line.rstrip('\n').split(' ')[1] for line in open(filelist)]
+  def __init__(self, datadir):
+    self.list = [line.rstrip('\n').split(' ')[1] for line in open(datadir+"/feats_test.scp")]
     self.collator = Collator('mix')
 
   def __len__(self):
@@ -108,7 +101,7 @@ class EnhBLSTM(nn.Module):
 
     self.lin = nn.Linear(600*2, 514)
 
-    self.bn2 = nn.BatchNorm1d(600*2)
+    self.bn = nn.BatchNorm1d(600*2)
 
   def init_hidden(self, batch_size):
     if self.gpuid > -1:
@@ -119,11 +112,23 @@ class EnhBLSTM(nn.Module):
               torch.randn(2*2, batch_size, 600))
 
   def forward(self, x):
+    # x: packed sequence of dim 257
+
     x, self.hidden = self.blstm(x, self.hidden)
+    # x: packed sequence of dim 600*2
+
     x, lens = pad_packed_sequence(x, batch_first=True)
-    x = self.bn2(x.permute(0,2,1).contiguous()).permute(0,2,1)
+    # x: tensor of shape (batch, seq_length, 600*2)
+
+    x = self.bn(x.permute(0,2,1).contiguous()).permute(0,2,1)
+    # x: tensor of shape (batch, seq_length, 600*2)
+
     x = self.lin(x)
+    # x: tensor of shape (batch, seq_length, 514)
+
     x = F.sigmoid(x)
+    # x: tensor of shape (batch, seq_length, 514)
+
     return x
 
 # define training pass
@@ -139,15 +144,22 @@ def compute_loss(model, batch_sample, plotdir=""):
   model.hidden = model.init_hidden(batch)
 
   mask_out = model(mix)
+  # mask_out: tensor of shape (batch, seq_length, 514)
 
   mixes, lens = pad_packed_sequence(mix, batch_first=True)
+  # mixes: tensor of shape (batch, seq_length, 257
   permutations1, lens = pad_packed_sequence(permute1, batch_first=True)
   permutations2, lens = pad_packed_sequence(permute2, batch_first=True)
+  # permutations*: tensor of shape (batch, seq_length, 514)
   lengths = lens.float().cuda()
   double_mix = torch.cat((mixes, mixes), dim=2)
+  # double_mix: tensor of shape (batch, seq_length, 514)
   masked = mask_out * double_mix
+  # masked: tensor of shape (batch, seq_length, 514)
   loss1 = torch.sum(loss_function(masked, permutations1).view(batch, -1), dim=1)
   loss2 = torch.sum(loss_function(masked, permutations2).view(batch, -1), dim=1)
+  # loss_function(masked, permutations*).view(batch, -1): tensor of shape (batch, seq_length*514)
+  # loss*: tensor of shape (batch)
 
   if plotdir:
     os.system("mkdir -p "+plotdir)
@@ -159,6 +171,7 @@ def compute_loss(model, batch_sample, plotdir=""):
       plot.plot_spec(permutations2[0].detach().cpu().numpy(), plotdir+'/Chosen_Permutation.png')
 
   return torch.mean(torch.min(loss1, loss2)/lengths/514)
+  # elt-wise min along batch between loss for each permutation
 
 # define test pass
 def compute_masks(model, batch_sample, out_dir):
