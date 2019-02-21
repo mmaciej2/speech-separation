021df43096ce7fd78c0bb2b4783832be151d7ac0
Matthew Maciejewski <mmaciej2@jhu.edu>
2019-02-21 14:14:07 -0500

    updated COE cpu cmd

--- cmd.sh
+++ cmd.sh
@@ -7,6 +7,7 @@ case $(hostname -d) in
   cm.gemini)
     export run_cmd="qsub -cwd -sync y -q all.q -l mem_free=4G,num_proc=1,h_rt=48:00:00"
     export run_cmd_nosync="qsub -cwd -q all.q -l mem_free=2G,num_proc=1,h_rt=48:00:00"
+    export cpu_cmd="-cwd -q all.q -l mem_free=2G,num_proc=1,h_rt=48:00:00"
     export train_cmd="-q gpu.q@@1080 -l gpu=1,mem_free=10G,h_rt=72:00:00,num_proc=1"
     export eval_cmd="-q gpu.q@@1080 -l gpu=1,mem_free=10G,h_rt=4:00:00,num_proc=1"
     export eval_all_cmd="-q gpu.q -l gpu=1,mem_free=10G,h_rt=4:00:00,num_proc=1"
