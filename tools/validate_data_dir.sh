e5a316f2ddd3bf940920499b4647fe4130aff31e
Matthew Maciejewski <mmaciej2@jhu.edu>
2019-02-21 14:10:19 -0500

    Added qsub batch feature extraction

--- tools/validate_data_dir.sh
+++ tools/validate_data_dir.sh
@@ -8,19 +8,37 @@ fi
 
 dir=$1
 
-if [ ! -f $dir/wav.scp ]; then
-  echo "No wav.scp file"
-  exit 1;
+if [ ! -f $dir/segments ]; then
+  echo "Using wav.scp for utterance list"
+  utt_file="$dir/wav.scp"
+  if [ ! -f $dir/wav.scp ]; then
+    echo "ERROR: No wav.scp file"
+    exit 1;
+  fi
+else
+  echo "Using segments for utterance list"
+  utt_file="$dir/segments"
+  if [ ! -f $dir/wav.scp ]; then
+    echo "WARNING: no wav.scp file"
+  else
+    nl=$(diff <( cut -d' ' -f1 $dir/wav.scp | sort -u ) <( cut -d' ' -f2 $dir/segments | sort -u ) | wc -l)
+    if [ "$nl" -ne "0" ]; then
+      echo "segments does not match wav.scp"
+      exit 1;
+    fi
+  fi
 fi
 
 feats=$(ls $dir/feats*.scp | awk -F'/' '{print $NF}')
 
 for file in $feats utt2num_spk utt2spk; do
   if [ -f $dir/$file ]; then
-    nl=$(diff <(cut -d' ' -f1 $dir/wav.scp) <(cut -d' ' -f1 $dir/$file) | wc -l)
+    nl=$(diff <(cut -d' ' -f1 $utt_file) <(cut -d' ' -f1 $dir/$file) | wc -l)
     if [ "$nl" -ne "0" ]; then
       echo "$file does not match wav.scp"
       exit 1;
     fi
   fi
 done
+
+echo "Data directory $dir is OK."
