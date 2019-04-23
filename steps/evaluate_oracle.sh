3ad3e68d927e0c22d721b939b1cde50aead64094
Matthew Maciejewski <mmaciej2@jhu.edu>
2019-04-23 17:58:13 -0400

    updated oracle evaluation to support hard mask

--- steps/evaluate_oracle.sh
+++ steps/evaluate_oracle.sh
@@ -8,7 +8,7 @@ if [ $# -le 0 ]; then
   echo "optional arguments:"
   echo "  --nj (num jobs)         <1>"
   echo "  --mj (max concur. jobs) <20>"
-  echo "  --hard-mask             <False>"
+  echo "  --hard-mask             <false>"
   echo "  --fft-dim               <512>"
   echo "  --step-size             <128>"
   echo "  --sample-rate           <8000>"
@@ -19,7 +19,7 @@ data_dir=$1
 shift 1
 nj=1
 mj=20
-hard_mask=False
+hard_mask=false
 fft_dim=512
 step_size=128
 sample_rate=8000
@@ -38,30 +38,39 @@ while true; do
   esac
 done
 
+[ "$hard_mask" != true ] || mask_opt="--hard-mask"
+if [ "$hard_mask" == true ]; then
+  mask_opt="--hard-mask"
+  eval_dir=oracle_hard_mask_eval
+else
+  mask_opt=""
+  eval_dir=oracle_soft_mask_eval
+fi
+
 if [ $nj -gt 1 ]; then
   tools/validate_data_dir.sh $data_dir || exit 1;
   tools/split_data_dir.sh $data_dir $nj
   qsub $cpu_cmd -sync y -t 1-$nj -tc $mj -j y -o $data_dir/split$nj/evaluate_oracle.log.\$TASK_ID \
     steps/evaluate_oracle.py $data_dir/split$nj \
-      --hard-mask $hard_mask \
+      $mask_opt \
       --fft-dim $fft_dim \
       --step-size $step_size \
       --sample-rate $sample_rate
   sleep 15
-  rm -rf $data_dir/oracle_eval
-  mkdir -p $data_dir/oracle_eval
+  rm -rf $data_dir/$eval_dir
+  mkdir -p $data_dir/$eval_dir
   for score_type in session source; do
     for metric in SDR SIR SAR; do
       for i in $(seq 1 $nj); do
-        cat $data_dir/split$nj/oracle_eval/${score_type}_${metric}s.txt.$i >> $data_dir/oracle_eval/${score_type}_${metric}s.tmp
+        cat $data_dir/split$nj/$eval_dir/${score_type}_${metric}s.txt.$i >> $data_dir/$eval_dir/${score_type}_${metric}s.tmp
       done
-      sort -u $data_dir/oracle_eval/${score_type}_${metric}s.tmp > $data_dir/oracle_eval/${score_type}_${metric}s.txt
-      rm $data_dir/oracle_eval/${score_type}_${metric}s.tmp
+      sort -u $data_dir/$eval_dir/${score_type}_${metric}s.tmp > $data_dir/$eval_dir/${score_type}_${metric}s.txt
+      rm $data_dir/$eval_dir/${score_type}_${metric}s.tmp
     done
   done
 else
   $run_cmd steps/evaluate_oracle.py $data_dir \
-    --hard-mask $hard_mask \
+    $mask_opt \
     --fft-dim $fft_dim \
     --step-size $step_size \
     --sample-rate $sample_rate
@@ -69,7 +78,7 @@ else
 fi
 
 for metric in SDR SIR SAR; do
-  cut -d' ' -f2- ${data_dir}/oracle_eval/source_${metric}s.txt | sed -e 's/ /\n/g' | \
-    awk -v f_out="${data_dir}/oracle_eval/${metric}_stats.txt" \
+  cut -d' ' -f2- ${data_dir}/$eval_dir/source_${metric}s.txt | sed -e 's/ /\n/g' | \
+    awk -v f_out="${data_dir}/$eval_dir/${metric}_stats.txt" \
       'BEGIN {max = -100; min = 100} {x += $0; y += $0^2; if($0 > max) {max = $0}; if($0 < min) {min = $0}} END {printf("Mean:\t%f\nStd:\t%f\nMax:\t%f\nMin:\t%f\n", x/NR, sqrt(y/NR-(x/NR)^2), max, min) > f_out}'
 done
